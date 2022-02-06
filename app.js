51b22e5ea57637c47a5f80f1567de6f9b4ca80d2
harsha <ggharshabandi@gmail.com>
2022-02-05 20:03:46 -0600

    Add up to express router

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log(err))
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
@@ -7,4 +14,6 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {console.log(`Listening on port ${port}`)});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+});
