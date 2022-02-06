3850b247118bf7a8f3c7aae2141a4d0d28b4107d
harsha <ggharshabandi@gmail.com>
2022-02-05 22:14:29 -0600

    Commiting before pushing to github

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log(err))
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: "harsha",
+        email: "harsha@gmail.com",
+        password: "password"
+    })
+    user.save()
     res.send("Hello world!");
 });
 
